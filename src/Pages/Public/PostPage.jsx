3a2d96c18e1b41839bb1bebdbe8c8d488ee6fc30
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2026-01-20 19:05:21 +0500

    the

--- src/Pages/Public/PostPage.jsx
+++ src/Pages/Public/PostPage.jsx
@@ -1,11 +1,82 @@
-import React from 'react'
+import React from "react";
+import PostHero from "../../components/PostHero";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 
-function PostPage() {
+let Base = import.meta.env.VITE_BASE_URL;
+
+function PostPages() {
+  let [inputValue, setInputValue] = useState("");
+  let [post, setPost] = useState([]);
+
+  useEffect(() => {
+    async function GetPOst() {
+      try {
+        let res = await fetch(`${Base}api/v1/articles/`);
+        if (!res.ok) {
+          throw new Error("Xatolik !");
+        }
+        let data = await res.json();
+        setPost(data);
+      } catch (error) {
+        toast("Xatolik");
+      }
+    }
+    GetPOst();
+  }, []);
+
+  let filtr = post.filter((item) => {
+    return item.content.toLowerCase().includes(inputValue.trim().toLowerCase());
+  });
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <PostHero inputValue={inputValue} setInputValue={setInputValue} />
+      <div className="container py-16">
+        <div className="  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {filtr.map((post) => {
+            return (
+              <div
+                key={post.id}
+                className="border-2 group border-gray-300 rounded-2xl duration-300 ease-in-out hover:-translate-y-1 hover:shadow-xl"
+              >
+                <div className="relative rounded-t-2xl overflow-hidden">
+                  <h2 className="bg-[#4346EF] z-2 py-0.5 px-2.5 rounded-2xl text-white absolute top-5 left-4">
+                    {post.category.name}
+                  </h2>
+                  <img
+                    className="rounded-t-2xl z-1 w-full h-full object-cover  transition-transform duration-500 group-hover:scale-110 "
+                    src={post.image}
+                    alt=""
+                  />
+                </div>
+                <div className="p-6">
+                  <div className="text-[#6B7280] mb-12px flex gap-2 items-center">
+                    <i className="fa-regular fa-calendar"></i>
+                    <p>{post.updated_at.slice(0, 10)}</p>
+                  </div>
+                  <h2 className="text-[#0F1729] group-hover:text-[#4346EF] transition text-[20px] font-bold mb-2">
+                    {post.title}
+                  </h2>
+                  <p className="text-[#6B7280] mb-4">{post.content}</p>
+                  <div className="text-[#4346EF]">
+                    <Link
+                      className="flex justify-start items-center gap-1 group-hover:gap-2.5"
+                      to={`/PostDetailsPages/${post.id}`}
+                    >
+                      Read more{" "}
+                      <i className="text-[16px] fa-solid fa-arrow-right"></i>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default PostPage
+export default PostPages;
