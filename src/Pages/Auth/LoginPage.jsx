ca047e1acd55299ad53e9da1fb25f11b36e0eff1
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2026-01-28 22:19:40 +0500

    the and

--- src/Pages/Auth/LoginPage.jsx
+++ src/Pages/Auth/LoginPage.jsx
@@ -2,11 +2,46 @@ import React from "react";
 import BlogyLogo from "../../assets/img/Link.svg";
 import { Link } from "react-router-dom";
 import LoginImg from "../../assets/img/LoginImg.png"
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function LoginPage() {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
+
+  let Base = import.meta.env.VITE_BASE_URL;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch(`${Base}/auth/login/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+           email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }),
+      })
+      if(!res.ok){
+        throw new Error("Login failed")
+      }
+
+      let data = await res.json();
+      console.log(data);
+      
+
+    } catch (error) {
+      toast(error.message)
+    }
+  }
+
   return (
     <div className="flex w-full h-screen">
-      <div className="w-1/2 flex flex-col justify-center ictems-center">
+      <div className="w-1/2 flex flex-col justify-center items-center">
         <div className="m-auto w-full max-w-md">
           <img className="mb-8 w-fit" src={BlogyLogo} alt="Blogy Logo" />
           <Link
@@ -19,14 +54,14 @@ function LoginPage() {
           <div className="py-6 px-8 w-full max-w-md border-2 border-gray-200 rounded-2xl ">
             <h2 className="text-[#0F1729] text-[30px] font-bold mb-2">Welcome Back</h2>
             <p className="text-[16px] text-[#6B7280] mb-6">Enter your credentials to access your account</p>
-            <form>
+            <form onSubmit={handleSubmit}>
               <label className="flex flex-col">
                 <span className="text-[14px] text-[#0F1729] font-medium mb-2">Email</span>
-                <input className="border-2 border-gray-200 rounded-lg p-2 outline-none mb-4" type="text" placeholder="name@example.com" />
+                <input ref={emailRef} className="border-2 border-gray-200 rounded-lg p-2 outline-none mb-4" type="text" placeholder="name@example.com" />
               </label>
               <label className="flex flex-col">
                 <span className="text-[14px] text-[#0F1729] font-medium mb-2">Password</span>
-                <input className="border-2 border-gray-200 rounded-lg p-2 outline-none mb-4" type="password" placeholder="********" />
+                <input ref={passwordRef} className="border-2 border-gray-200 rounded-lg p-2 outline-none mb-4" type="password" placeholder="********" />
               </label>
               <button
                 className="text-white cursor-pointer rounded-2xl w-full py-3 bg-[#4346EF]"
